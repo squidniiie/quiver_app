5096e7758bc3c3c130e0b1e7d0d5af5bee712817
Sydnie <sydniegreenwood@gmail.com>
2022-01-11 14:04:51 -0800

    commit

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -21,19 +21,26 @@ const Form = () => {
             email,
             password,
             confirmPassword
-        })
+        },
+            { withCredentials: true, }
+            // { headers: { 'Access-Control-Allow-Origin': 'http://localhost:3000' } }
+            // { headers: ["Accept"] }
+        )
             .then(res => {
                 history.push("/dashboard")
+                console.log(res)
+                console.log(res.data)
             })
-            .catch(err => {
-                const errorResponse = err.response.data.errors;
-                console.log(errorResponse)
-                const errorArr = [];
-                for (const key of Object.keys(errorResponse)) {
-                    errorArr.push(errorResponse[key].message)
-                }
-                setErrors(errorArr);
-            })
+            .catch(err => console.log(err))
+        // .catch(err => {
+        //     const errorResponse = err.response.data.errors;
+        //     console.log(errorResponse)
+        //     const errorArr = [];
+        //     for (const key of Object.keys(errorResponse)) {
+        //         errorArr.push(errorResponse[key].message)
+        //     }
+        //     setErrors(errorArr);
+        // })
     }
 
     return (
@@ -41,32 +48,32 @@ const Form = () => {
             <button className="btn btn-info" onClick={(e) => history.push('/dashboard')}>
                 Back to Dashboard
             </button>
-            <div className="container">
-                <div className="card col-3 bg-light">
-                    <h1 className="h1">Add User</h1>
+            <div className="shadow-xl bg-gray-50 rounded-lg p-4 m-4">
+                <div className="font-Quicksand text-center">
+                    <h1 className="text-2xl font-bold text-center">Add User</h1>
                     <form className="col" onSubmit={onSubmitHandler}>
                         {errors.map((err, index) => <p className="text-danger" key={index}>{err}</p>)}
-                        <p>
-                            <label>Username:</label><br />
-                            <input type="text" onChange={(e) => setUserName(e.target.value)} value={userName} />
+                        <label>Username:</label>
+                        <p className="flex items-center border-2 border-gray-50 shadow-md rounded-full bg-white" >
+                            <input className="flex-grow pl-1 bg-transparent outline-none text-sm sm:m-1 md:text-center sm:text-left text-gray-600 font-Quicksand bg-white" type="text" onChange={(e) => setUserName(e.target.value)} value={userName} />
                         </p>
-                        <p>
-                            <label>Image Url:</label><br />
-                            <input type="text" onChange={(e) => setImgUrl(e.target.value)} value={imgUrl} />
+                        <label>Image Url:</label>
+                        <p className="flex items-center border-2 border-gray-50 shadow-md rounded-full bg-white">
+                            <input className="flex-grow pl-1 bg-transparent outline-none text-sm sm:m-1 md:text-center sm:text-left text-gray-600 font-Quicksand bg-white" type="text" onChange={(e) => setImgUrl(e.target.value)} value={imgUrl} />
                         </p>
-                        <p>
-                            <label>Email:</label><br />
-                            <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
+                        <label>Email:</label>
+                        <p className="flex items-center border-2 border-gray-50 shadow-md rounded-full bg-white">
+                            <input className="flex-grow pl-1 bg-transparent outline-none text-sm sm:m-1 md:text-center sm:text-left text-gray-600 font-Quicksand bg-white" type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
                         </p>
-                        <p>
-                            <label>Password:</label><br />
-                            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        <label>Password:</label>
+                        <p className="flex items-center border-2 border-gray-50 shadow-md rounded-full bg-white">
+                            <input className="flex-grow pl-1 bg-transparent outline-none text-sm sm:m-1 md:text-center sm:text-left text-gray-600 font-Quicksand bg-white" type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
                         </p>
-                        <p>
-                            <label>Confirm Password:</label><br />
-                            <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
+                        <label>Confirm Password:</label>
+                        <p className="flex items-center border-2 border-gray-50 shadow-md rounded-full bg-white">
+                            <input className="flex-grow pl-1 bg-transparent outline-none text-sm sm:m-1 md:text-center sm:text-left text-gray-600 font-Quicksand bg-white" type="password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
                         </p>
-                        <input className="btn btn-info" type="submit" value="Add New User" onSubmit={(e) => history.push("/")} />
+                        <input className="bg-green-300 font-Montserrat text-lg font-bold text-white rounded-full mt-4 p-1 px-3 cursor-pointer active:scale-90 transform transition duration-500 hover:scale-90 hover:bg-green-400 hover:shadow-xl" type="submit" value="Add New User" onSubmit={(e) => history.push("/")} />
                     </form>
                 </div>
             </div>
--- server/server.js
+++ server/server.js
@@ -1,37 +1,61 @@
 // Validation and registration:
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
-// Middleware CORS API CALLS: 
-app.use((req, res, next) => {
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET", true);
-        return res.status(200).json({});
-    }
-    next();
-});
+const app = express();
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
+//  MIDDLEWARE:
+// Cookies:
 const cookieParser = require('cookie-parser');
 
 require('./config/mongoose.config');
 app.use(cookieParser());
 
-//  MIDDLEWARE:
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(express.json());
+// blocking cors errors:
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["*"],
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// }
+// app.use(cors(corsOptions)) // Use this after the variable declaration
 
+
+//  MIDDLEWARE:
+app.use(cors(
+    {
+        credentials: true,
+        origin: 'http://localhost:3000',
+        // headers: { "Access-Control-Allow-Origin": ["*"] },
+        methods: ["GET", "POST"]
+    }));
+
+//required for post request
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// routes:
 require('./routes/user.routes')(app);
 require('./routes/spot.routes')(app);
 
+
+// Middleware CORS-Manual API CALLS: 
+// app.use((req, res, next) => {
+//     if (req.method === "OPTIONS") {
+//         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET", true);
+//         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//         return res.status(200).end();
+//         // return res.status(200).json({});
+//     }
+//     next();
+// });
+
+//listen on port:
 app.listen(9000, () => {
     console.log("Listening at Port 9000")
 })
+
+
+
+
